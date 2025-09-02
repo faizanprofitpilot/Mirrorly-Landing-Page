2a9880457e0fca9afdf165b6c5671dd97835f863
Faizan Muhammad <faizan.m.dewan@gmail.com>
2025-09-02 12:02:10 -0400

    Remove static export - use standard Next.js deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  distDir: 'out',
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
   }
 }
 
